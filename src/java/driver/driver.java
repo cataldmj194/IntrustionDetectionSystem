a6af87634150417562b269074a3cf31a92dadad8
Michael Cataldo <cataldmj194@potsdam.edu>
2017-06-17 17:07:37 -0400

    Began parser and driver in java, going to use to read kddcup data into RDD for spark

---
+++ src/java/driver/driver.java
@@ -0,0 +1,13 @@
+/** Name: Michael Cataldo
+    Date: 6/17/2017
+    Project: IDS - driver
+*/
+
+public class driver {
+
+  public static void main(String[] args){
+
+  }
+
+}
+
---
+++ src/java/parser/parser.java
@@ -0,0 +1,16 @@
+/** Name: Michael Cataldo
+    Date: 6/17/2017
+    Project: IDS - Parser
+*/
+package parser;
+
+public class parser {
+
+  public static void main(String[] args){
+
+    
+
+  }
+
+}
+
